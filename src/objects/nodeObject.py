e8f64328a2afe77668fd2593664d1cd8d1b25b0e
Alex Velasco <131alexx@gmail.com>
2020-07-09 18:42:53 +0200

    [FIX] refactor node object

--- src/objects/nodeObject.py
+++ src/objects/nodeObject.py
@@ -1,7 +1,13 @@
-# Node abstract object to support conector and recognizing objects
+"""Node abstract object to support conector and recognizing objects
+"""
 
 
+# pylint: disable=R0903
 class Node:
+    """Abstract class inhereted by all components of an organism
+
+    TODO: Pass common methods to this class
+    """
+
     def __init__(self):
-        # self.ID = random.randint(0,100)
-        return 0
+        pass
